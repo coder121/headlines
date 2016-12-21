c101c3a13bbd907774331ab026537d15b4055391
Siddiq <siddiq235@gmail.com>
2016-12-20 17:37:33 -0800

    showing all news

--- headlines.py
+++ headlines.py
@@ -12,23 +12,27 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route("/")
 @app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news(publication="indiatoday"):
   return get_news(publication)
 
 
 
 def get_news(publication):
   feed = feedparser.parse(RSS_FEEDS[publication])
+  articles="""<html>
+    <body><h1>Headlines</h1>"""
   first_article = feed['entries'][0]
-  return """<html>
-    <body>
-        <h1>Headlines </h1>
-        <b>{0}</b> </ br>
+  all_articles=feed['entries']
+  for article in all_articles:
+    articles+="""
+       <h2>{0}</h2> </ br>
         <i>{1}</i> </ br>
         <p>{2}</p> </ br>
-    </body>
-</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-
+    </br> </br></br>""".encode('utf-8').format(article.get("title"), article.get("published"), article.get("summary"))
+  
+  articles+="""</body>
+            </html>"""
+  return articles 
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
