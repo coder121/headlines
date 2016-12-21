9efc88271d7b8002d994c5455910c9feeceaa6e2
Siddiq <siddiq235@gmail.com>
2016-12-20 23:42:27 -0800

    POST requests

--- headlines.py
+++ headlines.py
@@ -1,22 +1,26 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'indiatoday':'http://indiatoday.intoday.in/rss/article.jsp?sid=150'}
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-
-@app.route("/")
-@app.route("/<publication>")
-def get_news(publication="cnn"):
-  feed = feedparser.parse(RSS_FEEDS[publication])
-  return render_template("home.html", articles=feed['entries'])
+@app.route("/", methods=['GET', 'POST'])
+def get_news():
+        query = request.form.get("publication")
+        if not query or query.lower() not in RSS_FEEDS:
+                publication = "bbc"
+        else:
+                publication = query.lower()
+        
+        feed = feedparser.parse(RSS_FEEDS[publication])
+        return render_template("home.html",articles=feed['entries'])
 
 
 if __name__ == "__main__":
-  app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
